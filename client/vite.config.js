09ee5ee5e8025279c1711facaa33d690d1f19568
Shivanshu-Bhawsar <shivanshubhawsar3@gmail.com>
2024-11-03 11:42:23 +0530

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/',
+  base: "/", // Base path for your application; keep it '/' for root deployment
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // Allows you to use '@' as an alias for the 'src' directory
     },
+  },
+  build: {
+    outDir: "dist", // Specify the output directory for the build
   },
 });
